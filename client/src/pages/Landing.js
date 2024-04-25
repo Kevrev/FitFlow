fef781527962063787bf4d028704dfc3f1849f68
Kevin Small <kevsm@proton.me>
2024-04-25 11:31:41 -0700

    added adjusted banner image, added landing text placeholder and positioning

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -5,7 +5,12 @@ function Landing() {
     return (
         <div className="Landing">
             <div className="banner-container">
-                <div className="banner"></div>
+                <div className="banner">
+                    <div className="blurb-text">
+                        <h1>START YOUR JOURNEY TODAY</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
             </div>
         </div>  
     );
