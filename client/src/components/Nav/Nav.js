83376a35ddeea3525022fdbf4270f54215b73716
Kevin Small <kevsm@proton.me>
2024-04-29 11:54:36 -0700

    Building and styling navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,15 +5,25 @@ import './Nav.scss'
 function Nav() {
     return (
         <nav className="navbar-test">
-        <Link className="navbar-text-test" to="/">
-            Home
+        <Link className="website-title" to="/dashboard">
+            FitFlow
         </Link>
-        <Link className="navbar-text-test" to="/dashboard">
-            Dashboard
-        </Link>
-        <Link className="navbar-text-test" to="/profile">
-            Profile
-        </Link>
+        <div>
+            <Link to="/workout">
+                <button className="btn btn-primary">
+                    Start Workout
+                </button>
+            </Link>
+            <Link className="navbar-text-test" to="/dashboard">
+                Dashboard
+            </Link>
+            <Link className="navbar-text-test" to="/profile">
+                <i class="far fa-circle-user"></i>
+                FirstName
+            </Link>
+            <div class="vr" style={{height: "29px", backgroundColor: "#AAB4BE"}}></div>
+        </div>
+        
         </nav>
     );
     }
